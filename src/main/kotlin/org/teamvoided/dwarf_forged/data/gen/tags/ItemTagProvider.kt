9ffe58d914ab76670b5cf07cff876b31885bf133
theendercore <theendercore@gmail.com>
2024-07-21 16:46:40 +0300

    - ores tags end with an "s"

--- src/main/kotlin/org/teamvoided/dwarf_forged/data/gen/tags/ItemTagProvider.kt
+++ src/main/kotlin/org/teamvoided/dwarf_forged/data/gen/tags/ItemTagProvider.kt
@@ -17,18 +17,18 @@ class ItemTagProvider(
 
     fun copyBlockTags() {
         //Gem Ores
-        copy(DFBlockTags.RUBY_ORE, DFItemTags.RUBY_ORE)
-        copy(DFBlockTags.SAPPHIRE_ORE, DFItemTags.SAPPHIRE_ORE)
-        copy(DFBlockTags.KYANITE_ORE, DFItemTags.KYANITE_ORE)
-        copy(DFBlockTags.MOONSTONE_ORE, DFItemTags.MOONSTONE_ORE)
-        copy(DFBlockTags.JADE_ORE, DFItemTags.JADE_ORE)
-        copy(DFBlockTags.SMOKY_QUARTZ_ORE, DFItemTags.SMOKY_QUARTZ_ORE)
-        copy(DFBlockTags.EUCLASE_ORE, DFItemTags.EUCLASE_ORE)
-        copy(DFBlockTags.BORACITE_ORE, DFItemTags.BORACITE_ORE)
-        copy(DFBlockTags.TOPAZ_ORE, DFItemTags.TOPAZ_ORE)
-        copy(DFBlockTags.TOURMALINE_ORE, DFItemTags.TOURMALINE_ORE)
-        copy(DFBlockTags.SPINEL_ORE, DFItemTags.SPINEL_ORE)
-        copy(DFBlockTags.HEMATITE_ORE, DFItemTags.HEMATITE_ORE)
-        copy(DFBlockTags.CARNELIAN_ORE, DFItemTags.CARNELIAN_ORE)
+        copy(DFBlockTags.RUBY_ORE, DFItemTags.RUBY_ORES)
+        copy(DFBlockTags.SAPPHIRE_ORE, DFItemTags.SAPPHIRE_ORES)
+        copy(DFBlockTags.KYANITE_ORE, DFItemTags.KYANITE_ORES)
+        copy(DFBlockTags.MOONSTONE_ORE, DFItemTags.MOONSTONE_ORES)
+        copy(DFBlockTags.JADE_ORE, DFItemTags.JADE_ORES)
+        copy(DFBlockTags.SMOKY_QUARTZ_ORE, DFItemTags.SMOKY_QUARTZ_ORES)
+        copy(DFBlockTags.EUCLASE_ORE, DFItemTags.EUCLASE_ORES)
+        copy(DFBlockTags.BORACITE_ORE, DFItemTags.BORACITE_ORES)
+        copy(DFBlockTags.TOPAZ_ORE, DFItemTags.TOPAZ_ORES)
+        copy(DFBlockTags.TOURMALINE_ORE, DFItemTags.TOURMALINE_ORES)
+        copy(DFBlockTags.SPINEL_ORE, DFItemTags.SPINEL_ORES)
+        copy(DFBlockTags.HEMATITE_ORE, DFItemTags.HEMATITE_ORES)
+        copy(DFBlockTags.CARNELIAN_ORE, DFItemTags.CARNELIAN_ORES)
     }
 }
--- src/main/kotlin/org/teamvoided/dwarf_forged/data/tags/DFItemTags.kt
+++ src/main/kotlin/org/teamvoided/dwarf_forged/data/tags/DFItemTags.kt
@@ -7,23 +7,40 @@ import org.teamvoided.dwarf_forged.DwarfForged.id
 import org.teamvoided.dwarf_forged.util.tag
 
 object DFItemTags {
-    val RUBY_ORE = create("ruby_ore")
-    val SAPPHIRE_ORE = create("sapphire_ore")
-    val KYANITE_ORE = create("kyanite_ore")
-    val MOONSTONE_ORE = create("moonstone_ore")
-    val JADE_ORE = create("jade_ore")
-    val SMOKY_QUARTZ_ORE = create("smoky_quartz_ore")
-    val EUCLASE_ORE = create("euclase_ore")
-    val BORACITE_ORE = create("boracite_ore")
-    val TOPAZ_ORE = create("topaz_ore")
-    val TOURMALINE_ORE = create("tourmaline_ore")
-    val SPINEL_ORE = create("spinel_ore")
-    val HEMATITE_ORE = create("hematite_ore")
-    val CARNELIAN_ORE = create("carnelian_ore")
+    val RUBY_ORES = create("ruby_ores")
+    val SAPPHIRE_ORES = create("sapphire_ores")
+    val KYANITE_ORES = create("kyanite_ores")
+    val MOONSTONE_ORES = create("moonstone_ores")
+    val JADE_ORES = create("jade_ores")
+    val SMOKY_QUARTZ_ORES = create("smoky_quartz_ores")
+    val EUCLASE_ORES = create("euclase_ores")
+    val BORACITE_ORES = create("boracite_ores")
+    val TOPAZ_ORES = create("topaz_ores")
+    val TOURMALINE_ORES = create("tourmaline_ores")
+    val SPINEL_ORES = create("spinel_ores")
+    val HEMATITE_ORES = create("hematite_ores")
+    val CARNELIAN_ORES = create("carnelian_ores")
+
 
 
 
 
 
     fun create(name: String): TagKey<Item> = RegistryKeys.ITEM.tag(id(name))
+
+    val ALL_TAGS = listOf(
+        RUBY_ORES,
+        SAPPHIRE_ORES,
+        KYANITE_ORES,
+        MOONSTONE_ORES,
+        JADE_ORES,
+        SMOKY_QUARTZ_ORES,
+        EUCLASE_ORES,
+        BORACITE_ORES,
+        TOPAZ_ORES,
+        TOURMALINE_ORES,
+        SPINEL_ORES,
+        HEMATITE_ORES,
+        CARNELIAN_ORES
+    )
 }
