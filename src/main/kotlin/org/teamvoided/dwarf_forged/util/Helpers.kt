74edd5a9ecc9a9599720ac7416bc38b101a5c1c5
theendercore <theendercore@gmail.com>
2024-07-21 16:41:29 +0300

    - helpers

--- src/main/kotlin/org/teamvoided/dwarf_forged/util/Helpers.kt
+++ src/main/kotlin/org/teamvoided/dwarf_forged/util/Helpers.kt
@@ -1,7 +1,12 @@
 package org.teamvoided.dwarf_forged.util
 
+import net.minecraft.item.Item
+import net.minecraft.item.ItemConvertible
 import net.minecraft.registry.Holder
+import net.minecraft.registry.Registries
 import net.minecraft.registry.Registry
+import net.minecraft.registry.RegistryKey
+import net.minecraft.registry.tag.TagKey
 import net.minecraft.text.Text
 import net.minecraft.util.Identifier
 import org.teamvoided.dwarf_forged.DwarfForged.id
@@ -10,5 +15,15 @@ fun <T> Registry<T>.register(id: String, entry: T): T = Registry.register(this, 
 fun <T> Registry<T>.registerHolder(id: String, entry: T): Holder<T> = Registry.registerHolder(this, id(id), entry)
 fun <T> Registry<T>.register(id: Identifier, entry: T): T = Registry.register(this, id, entry)
 fun <T> Registry<T>.registerHolder(id: Identifier, entry: T): Holder<T> = Registry.registerHolder(this, id, entry)
+
+fun <T, R : Registry<T>> RegistryKey<R>.tag(id: Identifier) = TagKey.of(this, id)
+
+
+fun Item.toId() = Registries.ITEM.getId(this)
+val Item.path get() = Registries.ITEM.getId(this).path
+fun ItemConvertible.toId() = this.asItem().toId()
+
+val <T> TagKey<T>.path get() = this.id.path
+
 
 fun String.text() = Text.translatable(this)
