9f31dc044309f95d1d7a36541efc1ae04a53ed8e
theendercore <theendercore@gmail.com>
2024-07-21 16:42:47 +0300

    - initialised all the new datagen

--- src/main/kotlin/org/teamvoided/dwarf_forged/data/gen/DwarfForgedData.kt
+++ src/main/kotlin/org/teamvoided/dwarf_forged/data/gen/DwarfForgedData.kt
@@ -4,8 +4,12 @@ import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
 import net.minecraft.registry.RegistrySetBuilder
 import org.teamvoided.dwarf_forged.DwarfForged.log
+import org.teamvoided.dwarf_forged.data.gen.prov.BlockLootTableProvider
 import org.teamvoided.dwarf_forged.data.gen.prov.EnLangProvider
 import org.teamvoided.dwarf_forged.data.gen.prov.ModelProviders
+import org.teamvoided.dwarf_forged.data.gen.prov.RecipeProvider
+import org.teamvoided.dwarf_forged.data.gen.tags.BlockTagProvider
+import org.teamvoided.dwarf_forged.data.gen.tags.ItemTagProvider
 
 @Suppress("unused")
 class DwarfForgedData : DataGeneratorEntrypoint {
@@ -13,6 +17,16 @@ class DwarfForgedData : DataGeneratorEntrypoint {
         log.info("Hello from DataGen")
         val pack = gen.createPack()
 
+        // Tags
+        val blockTags = pack.addProvider(::BlockTagProvider)
+        pack.addProvider { o, r -> ItemTagProvider(o, r, blockTags) }
+
+
+        pack.addProvider(::BlockLootTableProvider)
+        pack.addProvider(::RecipeProvider)
+
+
+        // Assets
         pack.addProvider(::ModelProviders)
         pack.addProvider(::EnLangProvider)
     }
