489f2b6d258ecebf3ba2a50ca86349ca8f0fb8af
TheEnderCore <theendercore@gmail.com>
2025-06-25 12:26:38 +0300

    BuddingCrystalBlock update

--- src/main/kotlin/org/teamvoided/dwarf_forged/block/Crystals.kt
+++ src/main/kotlin/org/teamvoided/dwarf_forged/block/Crystals.kt
@@ -2,6 +2,7 @@ package org.teamvoided.dwarf_forged.block
 
 import net.minecraft.block.AmethystClusterBlock
 import net.minecraft.block.AmethystClusterBlock.FACING
+import net.minecraft.block.AmethystClusterBlock.WATERLOGGED
 import net.minecraft.block.Block
 import net.minecraft.block.BlockState
 import net.minecraft.block.BuddingAmethystBlock
@@ -10,6 +11,7 @@ import net.minecraft.server.world.ServerWorld
 import net.minecraft.util.math.BlockPos
 import net.minecraft.util.math.Direction
 import net.minecraft.util.random.RandomGenerator
+import kotlin.jvm.optionals.getOrNull
 
 class CrystalClusterBlock(settings: Settings) : AmethystClusterBlock(7.0f, 3.0f, settings)
 class LargeCrystalBudBlock(settings: Settings) : AmethystClusterBlock(5.0f, 3.0f, settings)
@@ -17,28 +19,32 @@ class MediumCrystalBudBlock(settings: Settings) : AmethystClusterBlock(4.0f, 3.0
 class SmallCrystalBudBlock(settings: Settings) : AmethystClusterBlock(3.0f, 4.0f, settings)
 
 
-@Suppress("MemberVisibilityCanBePrivate")
-class BuddingCrystalBlock(settings: Settings, val budList: List<Block>, private val growthChance: Int = 5) :
+open class BuddingCrystalBlock(settings: Settings, val budList: List<Block>, private val growthChance: Int = 5) :
     BuddingAmethystBlock(settings) {
     override fun randomTick(state: BlockState, world: ServerWorld, pos: BlockPos, random: RandomGenerator) {
-        if (random.nextInt(growthChance) == 0) {
-            val direction = Direction.random(random)
-            val offsetPos = pos.offset(direction)
-            val existingState = world.getBlockState(offsetPos)
-            var block: Block? = if (canGrowIn(existingState)) budList[0]
-            else if (budList.contains(existingState.block) && existingState.get(FACING) == direction) {
-                val idx = budList.indexOf(existingState.block)
-                if (idx != budList.lastIndex) {
-                    budList[idx + 1]
-                } else null
-            } else null
+        if (random.nextInt(growthChance) != 0) return
 
-            if (block != null) {
-                val blockState2 = block.defaultState
-                    .with(FACING, direction)
-                    .with(AmethystClusterBlock.WATERLOGGED, existingState.fluidState.fluid == Fluids.WATER)
-                world.setBlockState(offsetPos, blockState2)
-            }
+        val direction = Direction.random(random)
+        val offsetPos = pos.offset(direction)
+        val existingState = world.getBlockState(offsetPos)
+        val block = getState(existingState, direction) ?: return
+
+        val newState = block.defaultState
+            .with(FACING, direction)
+            .with(WATERLOGGED, existingState.fluidState.fluid == Fluids.WATER)
+        world.setBlockState(offsetPos, newState)
+    }
+
+    open fun getState(existingState: BlockState, direction: Direction): Block? {
+        if (canGrowIn(existingState)) return budList[0]
+        if (existingState.getOrEmpty(FACING).getOrNull() != direction) return null
+        if (!budList.contains(existingState.block)) return null
+
+        val idx = budList.indexOf(existingState.block)
+        if (idx != budList.lastIndex) {
+            return budList[idx + 1]
         }
+
+        return null
     }
 }
