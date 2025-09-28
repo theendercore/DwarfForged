15743df8052d51adb12b279f815fb0213f0cd30e
TheEnderCore <theendercore@gmail.com>
2025-09-28 14:44:37 +0300

    yeah, this broke things

--- src/main/java/org/teamvoided/dwarf_forged/mixin/ChiseledBookshelfBlockEntityMixin.java
+++ src/main/java/org/teamvoided/dwarf_forged/mixin/ChiseledBookshelfBlockEntityMixin.java
@@ -14,7 +14,6 @@ import org.spongepowered.asm.mixin.Debug;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
-@Debug(export = true)
 @Mixin(ChiseledBookshelfBlockEntity.class)
 public abstract class ChiseledBookshelfBlockEntityMixin extends BlockEntity {
 
